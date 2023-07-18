bf38704bbf17a5971c96f53398a189d4bc7fa4b6
kim7887 <povelitel641@gmail.com>
2023-07-19 01:17:12 +0500

    сделал задание 66

---
+++ tasc_66/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt (string message)
+{
+    System.Console.WriteLine(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int m = Prompt("Введите целое число M");
+int n = Prompt("Введите целое число N");
+
+int sumOfNumbers(int m, int mn)
+{
+    if (n == m) return n;
+    return m + sumOfNumbers(m + 1, n);
+}
+
+Console.WriteLine($"Сумма чисел равна: {sumOfNumbers(m, n)}");
