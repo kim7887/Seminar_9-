d3513c7150229089319cf0117370805f7895e0da
kim7887 <povelitel641@gmail.com>
2023-07-19 01:12:48 +0500

    сделал задание 65

---
+++ tasc_65/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿// Задача 65. Задайте значение M и N. Напишите программу, которая выдаёт все натуральные  числа от M до N.
+Console.WriteLine("Введите начальное число M:");
+int inputM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число N:");
+int inputN =Convert.ToInt32(Console.ReadLine());
+
+void Perechislenie(int LastNumber, int FirstNumber)
+{
+    if (LastNumber <= FirstNumber -1) return;
+    Perechislenie(LastNumber - 1, FirstNumber);
+    Console.Write(LastNumber + ",");
+}
+Perechislenie(inputN, inputM);
