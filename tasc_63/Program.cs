053fb574b50a79c09f7814419d1aebe702930774
kim7887 <povelitel641@gmail.com>
2023-07-18 23:55:59 +0500

    сделал задачу 63

---
+++ tasc_63/Program.cs
@@ -0,0 +1,11 @@
+﻿﻿// Задача 63. Задайте значение N. Напишите программу, которая выдаёт все натуральные  числа от 1 до N.
+Console.WriteLine("Введите целое  число N");
+int number=Convert.ToInt32(Console.ReadLine());
+
+void Perechislenie(int LastNumber)
+{
+    if (LastNumber <= 0) return;
+    Perechislenie(LastNumber - 1);
+    Console.Write(LastNumber + "");
+}
+Perechislenie(number);
